bf068fdf0a130c6d232d115638782f26fe93faca
Abszissex <rubSXkwZDZGqrr4>
2022-06-06 13:36:53 +0200

    Simplified Rust application

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use warp::{Filter, post};
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize)]
-struct Employee {
-    name: String,
-    rate: u32,
-}
+use warp::{Filter};
 
 
 #[tokio::main]
@@ -14,30 +7,10 @@ async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    // POST /employees/:rate  {"name":"Sean","rate":2}
-    let promote = warp::post()
-        .and(warp::path("employees"))
-        .and(warp::path::param::<u32>())
-        // Only accept bodies smaller than 16kb...
-        .and(warp::body::content_length_limit(1024 * 16))
-        .and(warp::body::json())
-        .map(|rate, mut employee: Employee| {
-            // adds the rate from the request to the current one
-            employee.rate = rate + employee.rate;
-            // Return a JSON object
-            warp::reply::json(&employee)
-        });
-
     let routes =
         warp::
         get()
-            .and(hello)
-        .or(
-            post()
-                .and(
-                    promote
-                )
-        );
+            .and(hello);
 
     let (host , port) = ([0,0,0,0], 3030);
     // let (host , port) = ([127,0,0,1], 3030);
